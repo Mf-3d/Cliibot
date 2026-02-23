164518b01f092da457b47492ca0c6e15030fe269
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2026-02-23 19:58:23 +0900

    🎨 メッセージのログをする位置を変更

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -5,9 +5,8 @@ export function registerMessageCreateEvent(client: Client, logger: Logger) {
   client.on("messageCreate", (message) => {
     if (message.author.bot) return;
 
-    logger.info(`💬 Message from ${message.author.tag}: ${message.content}`);
-
     if (message.content === "ping") {
+      logger.info(`💬 Message from ${message.author.tag}: ${message.content}`);
       message.reply("pong!");
     }
   });

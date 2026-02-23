1d6f8fb2b0cd2f34b9b406c8a618ad5f8ead1dbe
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2026-02-23 12:41:15 +0900

    ♲ ロガーをクラス化

--- src/utils/logger/index.ts
+++ src/utils/logger/index.ts
@@ -1,68 +1,113 @@
-import { colorize, format } from "./format";
+import chalk from "chalk";
 import { LOG_LEVEL_PRIORITY, LogLevel } from "./levels";
-import { normalize, NormalizedLog } from "./normalize";
 
-type LogOptions = {
-  emoji?: string;
-  fileOnly?: string;
+type NormalizedLog = {
+  message: string;
+  stack?: string
 };
 
-let currentLogLevel: LogLevel = "info";
-
-/**
- * @param level The minimum level to log.
- */
-export function setLogLevel(level: LogLevel) {
-  currentLogLevel = level;
+interface Writer {
+  write(level: LogLevel, text: string, prefix?: string): void;
 }
 
-/**
- * @param level Log Level.
- */
-function shouldLog(level: LogLevel): boolean {
-  return (
-    LOG_LEVEL_PRIORITY[level] >=
-    LOG_LEVEL_PRIORITY[currentLogLevel]
-  );
-}
+export class Logger {
+  private currentLogLevel: LogLevel = "info";
+  private normalizer: Normalizer;
+  private formatter: Formatter;
+  private writers: Writer[];
 
-/**
- * @param level Log Level.
- * @param normalized Normalized Log.
- * @param options Options such as prefix and file output.
- * @returns 
- */
-function output(level: LogLevel, normalized: NormalizedLog, options?: LogOptions) {
-  if (!shouldLog(level)) return;
-  
-  const formatted = format(level, normalized.message, options?.emoji);
+  constructor () {
+    this.normalizer = new Normalizer();
+    this.formatter = new Formatter();
+    this.writers = [ new ConsoleWriter() ];
+  }
 
-  if (normalized.stack) console.error(normalized.stack);
+  setLogLevel(level: LogLevel) {
+    this.currentLogLevel = level;
+  }
 
-  writeToConsole(level, formatted);
-  // writeToFile(level);
-}
+  private shouldLog(level: LogLevel): boolean {
+    return (
+      LOG_LEVEL_PRIORITY[level] >=
+      LOG_LEVEL_PRIORITY[this.currentLogLevel]
+    );
+  }
 
-function writeToConsole(level: LogLevel, text: string) {
-  const colored = colorize(level, text);
+  /**
+   * @param level Log Level.
+   * @param normalized Normalized Log.
+   * @returns 
+   */
+  private output (level: LogLevel, normalized: NormalizedLog) {
+    if (!this.shouldLog(level)) return;
 
-  if (level === "error") {
-    console.error(colored);
-  } else if (level === "warn") {
-    console.warn(colored);
-  } else {
-    console.log(colored);
+    const formatted = this.formatter.format(level, normalized.message);
+    this.writers.forEach(writer => writer.write(level, formatted));
+  }
+
+  info (message: string) {
+    this.output("info", this.normalizer.normalize(message));;
+  }
+
+  warn (message: string) {
+    this.output("warn", this.normalizer.normalize(message));;
+  }
+
+  error (message: string | Error) {
+    this.output("info", this.normalizer.normalize(message));;
   }
 }
 
-export const logger = {
-  info(message: string, options?: LogOptions) {
-    output("info", normalize(message), options);
-  },
-  warn(message: string, options?: LogOptions) {
-    output("warn", normalize(message), options);
-  },
-  error(err: Error | string, options?: LogOptions) {
-    output("error", normalize(err), options);
-  },
-};
+class Normalizer {
+  normalize(err: Error | string): NormalizedLog {
+    if (err instanceof Error) {
+      return {
+        message: err.message,
+        stack: err.stack,
+      }
+    } else {
+      return {
+        message: err,
+      }
+    }
+  }
+}
+
+class Formatter {
+  format(level: LogLevel, message: string): string {
+    const time = new Date().toISOString();
+    return [
+      `[${time}]`,
+      `[${level.toUpperCase()}]`,
+      message
+    ].join(" "); // e.g. "[] [INFO] Hello, nice to meet you!"
+  }
+}
+
+class ConsoleWriter implements Writer {
+  write(level: LogLevel, text: string) {
+    const colored = this.colorize(level, text);
+  
+    if (level === "error") {
+      console.error(colored);
+    } else if (level === "warn") {
+      console.warn(colored);
+    } else {
+      console.log(colored);
+    }
+  }
+
+  // 色を付ける
+  private colorize(level: LogLevel, text: string) {
+    switch (level) {
+      case "info":
+        return chalk.blue(text);
+      case "warn":
+        return chalk.yellow.bold(text);
+      case "error":
+        return chalk.red.bold(text);
+      default:
+        return chalk.gray(text);
+    }
+  }
+}
